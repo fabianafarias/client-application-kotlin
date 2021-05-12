6fd3e74279c39d028af7e77b9dd119ba8aaf3b3a
fabiana farias <farias.fabianam@gmail.com>
2021-05-11 22:26:42 -0300

    Finalizando com o método getContentProvider

--- app/src/main/java/com/fabianafarias/applicationcontentproviderclient/MainActivity.kt
+++ app/src/main/java/com/fabianafarias/applicationcontentproviderclient/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.fabianafarias.applicationcontentproviderclient
 
+import android.annotation.SuppressLint
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +21,24 @@ class MainActivity : AppCompatActivity() {
 
         notesRefreshButton = findViewById(R.id.client_button_refresh)
         notesRecycler = findViewById(R.id.client_list)
+        getContentProvider()
 
-        notesRefreshButton.setOnClickListener {  }
+        notesRefreshButton.setOnClickListener { getContentProvider() }
+    }
+
+    @SuppressLint("Recycle")
+    private fun getContentProvider() {
+        try {
+            val url = "content://com.fabianafarias.applicationcontentprovider.provider/notes"
+            val data = Uri.parse(url)
+            val cursor: Cursor? =
+                contentResolver.query(data, null, null, null, "title")
+            notesRecycler.apply {
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                adapter = ClientAdapter(cursor as Cursor)
+            }
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
     }
 }
